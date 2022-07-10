93faad1303ae394f574396d774c9e5242e71b828
JudeSavio <judealphonsesavio@gmail.com>
2022-07-10 21:57:35 +1000

    Rendering & Re-Rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
 import logo from './logo.svg';
 import './App.css';
+import { type } from '@testing-library/user-event/dist/type';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters : [],
+      searchField : ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState( () => {
+      return {monsters:users}
+    }));
+  }
+
+  searchChange = (event) => {
+    var searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {searchField};
+    });
+  }
+
+  render () {
+    const {monsters , searchField} = this.state;
+    const {searchChange} = this;
+    var filtered_Monsters = monsters.filter( (monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+    return (
+      <div className="App">
+        <input 
+        className="search-box" 
+        type="search" 
+        placeholder='enter monsters' 
+        onChange={searchChange}
+        />
+        {
+          /*
+          filtered_Monsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1> 
+          })
+          */
+        }
+        <CardList monsters={filtered_Monsters}/> 
+      </div>
+    );
+  }
 }
 
 export default App;
